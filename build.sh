6389f24589a63ac8b9dc6591906d04edc900baaf
naphattharawat <naphattharawat@gmail.com>
2021-03-14 22:26:34 +0700

    v1.1.21

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+echo -n "version v.x.x.x";
+read;
 protoc --go_out=./ --go-grpc_out=./ *.proto
-git pull
+git add .
+git commit -m "${REPLY}"
+git pull 
+git push origin main
+git tag ${REPLY}
+git push origin ${REPLY}
 export latest="$(git describe --tags $(git rev-list --tags --max-count=1))"
 curl https://proxy.golang.org/github.com/moph-gateway/his-proto/@v/$latest.info
